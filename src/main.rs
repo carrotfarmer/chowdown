a353f810f97050323b302c21df7ccc1e173dceb0
Shine Chang <shine00chang@gmail.com>
2023-10-18 12:21:06 -0700

    (impl) bold & italics fix

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use nom::branch::alt;
-use nom::bytes::complete::{is_a, is_not, tag};
-use nom::character::complete::{line_ending, not_line_ending};
+use nom::bytes::complete::{is_not, tag};
+use nom::character::complete::{line_ending, not_line_ending, char, multispace0};
 use nom::combinator::map_res;
 use nom::error::Error;
 use nom::multi::{many0, many1};
@@ -13,44 +13,53 @@ enum Element {
     Heading { level: u32, text: String },
     Divider,
     Blockquote { text: String },
-    Bold { text: String },
+    Bold { text: String  },
     Italics { text: String },
     PlainText { text: String },
 }
 
 fn headings(input: &str) -> IResult<&str, Element> {
-    // Add a function to convert -- map -- the parsed string into our Enum.
     map_res(
-        /* Bundles two parsers together, and outputs (A, B),
-         * the outputs of the two parsers respectively.
-         */
-        pair(
-            many1(is_a::<&str, &str, _>("#")),
-            not_line_ending::<&str, _>,
+        terminated(
+            pair(
+                many1(char::<&str, _>('#')),
+                not_line_ending,
+            ),
+            line_ending
         ),
         |(hashtags, text)| -> Result<Element, Error<&str>> {
             // Heading "level" (size) is defined by the amount of '#'s
             let level = hashtags.len() as u32;
             Ok(Element::Heading {
                 level,
-                text: text.to_owned(),
+                text: text.trim().to_owned(),
             })
         },
     )(input)
 }
 
 fn divider(input: &str) -> IResult<&str, Element> {
-    map_res(tag("---"), |_| -> Result<Element, Error<&str>> {
+    map_res(tag("---\n"), |_| -> Result<Element, Error<&str>> {
         Ok(Element::Divider)
     })(input)
 }
 
 fn blockquote(input: &str) -> IResult<&str, Element> {
     map_res(
-        pair(is_a::<&str, &str, _>("> "), not_line_ending::<&str, _>),
-        |(_, text)| -> Result<Element, Error<&str>> {
+        many1(
+            delimited(char('>'), not_line_ending, line_ending),
+        ),
+        |texts| -> Result<Element, Error<&str>> {
+            let text = texts
+                .into_iter()
+                .fold(String::new(), |mut a, line: &str| {
+                    a.push_str(line.trim());
+                    a.push_str("\r\n");
+                    a
+                });
+
             Ok(Element::Blockquote {
-                text: text.to_owned(),
+                text
             })
         },
     )(input)
@@ -58,10 +67,10 @@ fn blockquote(input: &str) -> IResult<&str, Element> {
 
 fn bold(input: &str) -> IResult<&str, Element> {
     map_res(
-        delimited(tag("**"), is_not("**"), tag("**")),
+        delimited(tag("**"), plain_text, tag("**")),
         |text: &str| -> Result<Element, Error<&str>> {
             Ok(Element::Bold {
-                text: text.to_owned(),
+                text: text.to_owned()
             })
         },
     )(input)
@@ -69,24 +78,17 @@ fn bold(input: &str) -> IResult<&str, Element> {
 
 fn italics(input: &str) -> IResult<&str, Element> {
     map_res(
-        delimited(tag("*"), is_not("*"), tag("*")),
+        delimited(tag("*"), plain_text, tag("*")),
         |text: &str| -> Result<Element, Error<&str>> {
             Ok(Element::Italics {
-                text: text.to_owned(),
+                text: text.to_owned()
             })
         },
     )(input)
 }
 
-fn plain_text(input: &str) -> IResult<&str, Element> {
-    map_res(
-        not_line_ending::<&str, _>,
-        |text: &str| -> Result<Element, Error<&str>> {
-            Ok(Element::PlainText {
-                text: text.to_owned(),
-            })
-        },
-    )(input)
+fn plain_text(input: &str) -> IResult<&str, &str> {
+    is_not("*\n\r")(input)
 }
 
 fn main() {
@@ -98,24 +100,41 @@ Hello!
 divider?
 ---
 divided.
-> blockquote deez nuts
+> blockquote
+> deez
+> nuts
 **bold text**
 *italic text*
 hello **bold** world
-";
+    ".trim();
+
 
     // Produces a vector of elements
     let (residual, elements): (&str, Vec<_>) = many0(
-        /* Must end with a "line ending" (/r or /n) character.
-         * We use 'terminated()' here because it does not return the terminator,
-         * only the result of the first parser.
-         */
-        terminated(
-            alt((headings, divider, blockquote, bold, italics, plain_text)),
-            line_ending,
-        ),
+        /* Wrapped in "multispace0" to remove newlines & spaces */
+        delimited(
+            multispace0,
+            alt((
+                headings,
+                divider,
+                blockquote,
+                bold,
+                italics,
+
+                /* plaintext, but mapped into an element */
+                map_res(
+                    plain_text,
+                    |text: &str| -> Result<Element, Error<&str>> {
+                        Ok(Element::PlainText {
+                            text: text.to_owned(),
+                        })
+                    },
+                )
+            )),
+            multispace0
+        )
     )(string)
-    .unwrap();
+        .unwrap();
 
     if residual.len() != 0 {
         println!("should not be any residual.");
@@ -123,13 +142,8 @@ hello **bold** world
         panic!();
     }
 
-    /* Expects:
-     * Element::Heading{ level: 1, text: "Big heading" }
-     * Element::PlainText{ text: "Hello!" }
-     * Element::Heading{ level: 3, text: "Small heading" }
-     * Element::PlainText{ text: "divider?" }
-     * Element::Divider
-     * Element::PlainText{ text: "divided." }
-     */
-    elements.iter().for_each(|e| println!("{:?}", e));
+    // Print Result
+    elements
+        .iter()
+        .for_each(|e| println!("{:?}", e));
 }
