5e43e29086de5bd9a2eff7c931c3c095697a1e4f
Shine Chang <shine00chang@gmail.com>
2023-10-23 20:34:41 -0700

    (fix) restructure

--- src/generator.rs
+++ src/generator.rs
@@ -1,35 +1,31 @@
-use crate::parser::Element;
+use crate::{Element, Node};
 
-pub struct Generator {
-    elements: Vec<Element>,
+
+pub fn generate(nodes: Vec<Node>) -> String {
+    nodes
+        .iter()
+        .fold(String::new(), |mut out, node| {
+            out.push_str(&node.gen());
+            out
+        })
 }
 
-impl Generator {
-    pub fn new(elements: Vec<Element>) -> Self {
-        Self { elements }
-    }
-
-    pub fn generate(&self) -> String {
-        let mut output = String::new();
-
-        for element in &self.elements {
-            match element {
-                Element::PlainText { text } => output.push_str(&format!("<p>{}</p>", text)),
-                Element::Bold { text } => output.push_str(&format!("<b>{}</b>", text)),
-                Element::Italics { text } => output.push_str(&format!("<i>{}</i>", text)),
-                Element::Blockquote { text } => {
-                    output.push_str(&format!("<blockquote>{}</blockquote>", text))
-                }
-                Element::Heading { level, text } => {
-                    output.push_str(&format!("<h{}>{}</h{}>", level, text, level))
-                }
-                Element::Divider => output.push_str("<hr />"),
-                Element::InlineCode { text } => output.push_str(&format!("<code>{}</code>", text)),
-                Element::Strikethrough { text } => output.push_str(&format!("<s>{}</s>", text)),
+use Element::*;
+impl Node {
+    fn gen (&self) -> String {
+        match &self.element {
+            PlainText { text } => format!("<p>{}</p>", text),
+            Bold { text } => format!("<b>{}</b>", text),
+            Italics { text } => format!("<i>{}</i>", text),
+            Blockquote { text } => {
+                format!("<blockquote>{}</blockquote>", text)
             }
+            Heading { level, text } => {
+                format!("<h{}>{}</h{}>", level, text, level)
+            }
+            Divider => "<hr />".to_owned(),
+            InlineCode { text } => format!("<code>{}</code>", text),
         }
-
-        return output;
     }
 }
 
@@ -69,8 +65,8 @@ mod test {
             },
         ];
 
-        let generator = Generator::new(tokens);
-        let html = generator.generate();
+        let tree = crate::parser::parse(tokens);
+        let html = generate(tree);
 
         let expected = r#"<h1>Big heading</h1><p>Hello!</p><h3>Small heading</h3><p>divider?</p><hr /><p>divided.</p><b>bold text</b><i>italic text</i><s>strikethru text</s>"#;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
-mod generator;
+mod lexer;
 mod parser;
-
-use generator::Generator;
-use parser::Parser;
+mod generator;
 
 use wasm_bindgen::prelude::*;
 
@@ -17,13 +15,40 @@ macro_rules! console_log {
     ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
+
+//Logical representation of markdown elements.
+#[derive(Debug, PartialEq, Eq)]
+pub enum Element {
+    Heading { level: usize, text: String },
+    Divider,
+    Blockquote { text: String },
+    Bold { text: String },
+    Italics { text: String },
+    PlainText { text: String },
+    InlineCode { text: String },
+}
+
+
+#[derive(Debug)]
+pub struct Node {
+    element: Element,
+    children: Vec<Self>
+}
+
+
 #[wasm_bindgen]
 pub fn parse(markdown: String) -> String {
+
+    #[cfg(target_family = "wasm")]
     console_log!("hello");
-    let elements = Parser::new(markdown).parse();
+
+    let elements = lexer::parse(markdown);
     println!("{:?}", elements);
 
-    let html = Generator::new(elements).generate();
+    let tree = parser::parse(elements);
+    println!("{:?}", tree);
+
+    let html = generator::generate(tree);
 
     html
 }
